19496760dc6ed19f5b1afa0d843967158a9f60f4
Rafael Gabriel Arceo <arceo.rafaelgabriel@protonmail.com>
2022-02-12 21:50:46 +0800

    Working commit.

--- src/source/main.c
+++ src/source/main.c
@@ -10,9 +10,10 @@ Expected output would be an equation where the two constants a_0 and a_1 are cal
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 
-#define MAX_BUFFER_SIZE 8
+#define MAX_BUFFER_SIZE 16
 #define MAX_INT_BUFFER_SIZE 1024
 
 float *a1_calculateTerm1(float* x, float* y, int n, float* result);
@@ -29,30 +30,54 @@ int main(int argc, char **argv){
 
     float* ind = malloc(MAX_INT_BUFFER_SIZE * sizeof(float));
     float* dep = malloc(MAX_INT_BUFFER_SIZE * sizeof(float));
+    float a0 = 0;
+    float a1 = 0;
     int arrPointer = 0;
     int numberOfPoints = 0; 
 
-    char headers[1][2]; // headers assumes that the input headers are only one variable e.g. x, y
+    char* headers = malloc((sizeof(char) * MAX_BUFFER_SIZE));
     FILE *fp = fopen(argv[1], "r"); 
     if(fp == NULL){
         printf("Error: File not found.");
         return -1;
     } 
 
-    char* stringBuffer = malloc((char) MAX_BUFFER_SIZE);
-    fgets(stringBuffer, MAX_BUFFER_SIZE, fp); // reads the first line of the file.
-    headers[0][0] = *stringBuffer;
-    headers[0][1] = *(stringBuffer + 2); 
+    char* stringBuffer = malloc(MAX_BUFFER_SIZE);
+    fgets(stringBuffer, MAX_BUFFER_SIZE, fp); // reads the first line of the file
+    *(headers) = *(stringBuffer);
+    *(headers + 1) = *(stringBuffer + 2);
 
-    printf("Independent Variable: %c, Dependent Variable: %c. Calculating linear regression equation. \n", headers[0][0], headers[0][1]);
+    printf("Independent Variable: %c, Dependent Variable: %c. Calculating linear regression equation. \n", *headers, *(headers + 1));
 
+    /*
+    
+    TODO: Read line until ",". Parse entire function into a float. 
+    fgets reads entire string. Go through entire string until , is noticed. 
+    
+    */
     while(fgets(stringBuffer, MAX_BUFFER_SIZE, fp)){
-        sscanf((stringBuffer), "%f", (ind + arrPointer));
-        sscanf((stringBuffer + 2), "%f", (dep + arrPointer));
+        char *commaLocation = strchr(stringBuffer, (int) ',');
+        char *endLocation = strchr(stringBuffer, (int) '\r'); // find memory location of terminating character
+        int zeroToCommaDifference = abs((stringBuffer - commaLocation)); // Get the difference of memory locations
+        int commaToEndDifference = abs((stringBuffer - endLocation));
+
+        char* indBuffer = malloc(sizeof(char) * zeroToCommaDifference);
+        char* depBuffer = malloc(sizeof(char) * commaToEndDifference);
+
+        indBuffer = strncpy(indBuffer, stringBuffer, zeroToCommaDifference);
+        depBuffer = strncpy(depBuffer, (stringBuffer + zeroToCommaDifference + 1), commaToEndDifference);
+
+        sscanf(indBuffer, "%f", (ind + arrPointer)); 
+        sscanf(depBuffer, "%f", (dep + arrPointer)); 
+
+        free(indBuffer);
+        free(depBuffer);
+
         arrPointer++;
         numberOfPoints++; // one line represents one data point. Increment to find N. 
     }
 
+    free(stringBuffer); // release stringBuffer
 
     //calculate a0     
     float* a0Term1 = malloc(sizeof(float));
@@ -72,14 +97,22 @@ int main(int argc, char **argv){
     a1_calculateTerm4(ind, numberOfPoints, a1Term4);
 
     a0_calculateTerm1(ind, dep, numberOfPoints, a0Term1);
+    a0_calculateTerm2(ind, dep, numberOfPoints, a0Term2);
 
+    a0 = (*a0Term1 - *a0Term2)/(*a1Term3 - *a1Term4);
+    a1 = (*a1Term1 - *a1Term2)/(*a1Term3 - *a1Term4);
+
+    printf("Resulting equation: y = % 0.5f + %0.5fx", a0, a1);
+    
     return 0;
 }
 
 float *a1_calculateTerm1(float* x, float* y, int n, float* result){
+    float bufferX = 0;
+    float bufferY = 0; 
     for(int i = 0; i < n; i++){
-        float bufferX = *(x + i);
-        float bufferY = *(y + i);
+        bufferX = *(x + i);
+        bufferY = *(y + i);
         *result += bufferX*bufferY;     
     }
     *result *= n;
@@ -101,6 +134,7 @@ float *a1_calculateTerm3(float* x, int n, float *result){
     for(int i = 0; i < n; i++){
         *result += powf(*(x + i), 2.0);
     }
+    *result *= n; 
     return result; 
 }
 
@@ -116,18 +150,20 @@ float *a0_calculateTerm1(float* x, float* y, int n, float* result){
     float bufferX = 0;
     float bufferY = 0;
     for(int i = 0; i < n; i++){
-        bufferX += powf(*(x + i), 2);
+        bufferX += powf(*(x + i), 2.0);
         bufferY += *(y + i);
     }
     *result = bufferX * bufferY;
     return result; 
 }
-float *a0_calculateTerm2(float* x, float* y, int n,float* result){
 
-}
-float *a0_calculateTerm3(float* x, int n, float *result){
-
-}
-float *a0_calculateTerm4(float* x, int n, float* result){
-
+float *a0_calculateTerm2(float* x, float* y, int n, float* result){
+    float bufferX = 0;
+    float bufferXY = 0;
+    for(int i = 0; i < n; i++){
+        bufferX += *(x + i);
+        bufferXY += *(x + i) * *(y + i);
+    }
+    *result = bufferX * bufferXY; 
+    return result; 
 }
